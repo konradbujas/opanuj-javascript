900a70cae329452465ad9d52a65383ac599727c6
Konrad Bujas <kondziobu@gmail.com>
2021-11-16 09:30:10 +0100

    arrays methods

---
+++ 1x10 metody tablic - arrays methods.js
@@ -0,0 +1,27 @@
+
+
+// Funkcje tablic - metody to funkcje, które wykonywane są na obiektach, a tablice są obiektami
+
+const arr = [1, 2, 3, 4, 16, 5];
+
+// REVERSE - odwrócenie tablicy
+arr.reverse();
+arr;
+    // dochodzi tutaj do zmodyfikowania tablicy oryginalnej pomimo tego, że zadeklarowana jest jako 'const' (które nie pozwala nadpisywać wartości zmiennych) - czyli zawartość tablicy może się zmieniać, ale musi pozostać tablicą [taka sytuacja ma miejsce w przypadku typów złożonych jak tablice oraz obiekty]
+
+
+// SORT - sortowanie elementów, ale domyślnie traktuje wartości jako tekst i wtedy np. 16 będzie przed 2, bo zostają porównane pierwsze znaki
+arr.sort();
+arr;
+    // nie jest to najlepsza metoda sortowania, więc trzeba to zrobić w inny sposób:
+
+// zdefiniujemy sobie nową funkcję, a jej wynik przekażemy do funkcji sort - nazywamy to 'callback' - funkcja, która jest przekazywana od innej funkcji jako argument po to, aby można ją było później wykonać - czyli funkcja sortAsc zostanie wykonana wewnątrz funkcji .sort()
+arr.sort(sortAsc);
+
+function sortAsc() {
+
+}
+    // funkcja .sort() przyjmuje dwa argumenty, czyli oznaczone tu a oraz b. Tak naprawdę to są wartości 'poprzednia' oraz 'następna'. Czyli ten callback wykona się dla każdej pary znajdującej się w  tablicy - a porównanie to będzie odbywało się od końca.
+function sortAsc(prev, next) {
+
+    }
