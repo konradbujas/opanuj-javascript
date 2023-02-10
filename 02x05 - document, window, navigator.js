156ec9080fbcbc65c677cd3c1676980aea4254f6
Konrad Bujas <kondziobu@gmail.com>
2023-02-10 13:46:58 +0100

    window

--- 02x05 - document, window, navigator.js
+++ 02x05 - document, window, navigator.js
@@ -28,3 +28,27 @@ option.value = 1;
 select.append(option);
 form.appendChild(select);
 
+// Element nadrzędny
+
+const grandParent = document.parentNode;
+console.log(grandParent);
+// pokazuje null, bo nie istnieje document nadrzędny od document ;) document to element, od którego wszystko się zaczyna - nie ma nic "wyżej"
+
+
+// Interfejs location
+
+console.log(document.location);
+// mamy tutaj m.in property search, dzięki którym możemy zczytywać tzw. query params
+
+// Typ obiektu document
+
+const typeName = document.constructor.name
+console.log(typeName);
+// zwraca nam HTMLDocument - po wpisaniu w wyszukiwarkę otrzymamy wszystkie możliwe funkcje do wykonania na tym obiekcie oraz właściowości
+
+
+/* WINDOW */
+
+// Window to interfejs, który reprezentuje okno, w którym otwarta jest dana strona internetowa.
+// Zawiera opisywany wcześniej obiekt document
+// Udostępnia on m.in. informacje związane z rozmiarem okna, metody umożliwiające otwieranie okien potomnych, API takie jak localStorage oraz sessionStorage czy dostęp do tzw. custom elementów zarejstrowanych w kontekście danego okna
