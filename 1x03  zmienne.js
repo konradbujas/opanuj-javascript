b6d8bc4c369a89d8d30031984b16f1669e13f279
Konrad Bujas <kondziobu@gmail.com>
2023-02-15 11:44:01 +0100

    variables

--- 1x03  zmienne.js
+++ 1x03  zmienne.js
@@ -1,18 +1,33 @@
 // [1x3] zmienne
 
+// Zmienna to sposób w jaki program zapamiętuje informacje.
+// Zmienna służy do przechowywania informacji pod wskazaną nazwą - tzw.identyfikatorem, a samą informację nazywamy wartością.
+
+// Aby móc korzystać ze zmiennej musimy ją najpierw utworzyć, inaczej mówiąc zadeklarować i w JavaScripcie robimy to za pomocą var / let / const.
+
+// VAR - charakterystyczna dla starego JS, ale już jej nie używamy, choć nadal można się z nią spotkać. Deklaruje zmienną i opcjonalnie ustawia jej wartość - domyślnie 'undfefined'
+
+// LET - deklaruje zmienną i opcjonalnie ustawia jej wartość, jednak tak utworzona zmienna posiada zakres blokowy. (różnica pomiędzy let oraz var).
+
+// CONST - deklaruje zmienną, posiadającą zakres blokowy. Jej wartość NIE MOŻE zostać nadpisana. Jest tak na prawdę stałą. Inaczej mówiąc, gdy tworzymy zmienną musimy od razu przypisać do niej wartość, która w przyszłości nie będzie mogła być zmieniona.
+// W praktyce wygląda to tak, że słowo kluczowe 'const' wykorzystujemy praktycznie za każdym razem, gdy deklarujemy jakąś zmienną i tylko w sytuacji, kiedy wartość tej zmiennej może się zmienić - wtedy używamy słowa kluczowego 'let'.
+
+// Słowa kluczowego 'var' raczej nie używamy. Są tylko pojedyńcze przypadki, kiedy można go wykorzystać, natomiast dalej jest to bardzo kontrowersyjne i większość programistów tego nie poleca.
+
+// const home;
+// console.log(home); // SyntaxError: Unexpected token (przy const musimy przypisać wartość)
+
+// Zmienne zadeklarowane z wykorzystaniem słowa kluczowego 'const' są tak na prawdę stałymi, czyli takimi specjalnymi zmiennymi, których wartość nie może zostać zmieniona.
+
 var name = "Adam";
 console.log(name);
+
 name = "Marcin";
 console.log(name);
 var name = "Przemek";
 console.log(name);
 
-// przy deklarowaniu zmiennej słowem
-// kluczowym var istneje ryzyko jej
-// nadpisania, bądź zdeklarowania
-// ponownie jak na powyższym przykładzie
-// przy rozbudowanym kodzie istnieje ryzyko,
-// że nie będziemy wiedzieć, że taka zmienna już istnieje
+// przy deklarowaniu zmiennej słowem kluczowym var istneje ryzyko jej nadpisania, bądź zadeklarowania ponownie jak na powyższym przykładzie przy rozbudowanym kodzie istnieje ryzyko, że nie będziemy wiedzieć, że taka zmienna już istnieje
 
 var name2;
 name2;
