263c0b191eca5def94dbcc88cc9e5a8ca899247b
Konrad Bujas <kondziobu@gmail.com>
2023-02-16 12:38:35 +0100

    variables - block scope

--- 1x03  zmienne.js
+++ 1x03  zmienne.js
@@ -27,7 +27,9 @@ console.log(name);
 var name = "Przemek";
 console.log(name);
 
-// przy deklarowaniu zmiennej słowem kluczowym var istneje ryzyko jej nadpisania, bądź zadeklarowania ponownie jak na powyższym przykładzie przy rozbudowanym kodzie istnieje ryzyko, że nie będziemy wiedzieć, że taka zmienna już istnieje
+// Jak widać wszystko jest tutaj w porządku i program działa, natomiast przy deklarowaniu zmiennej słowem kluczowym var istneje ryzyko jej nadpisania, bądź zadeklarowania ponownie jak na powyższym przykładzie przy rozbudowanym kodzie istnieje ryzyko, że nie będziemy wiedzieć, że taka zmienna już istnieje
+
+// m.in z tego powodu wykorzystywanie słowa kluczowego 'var' jest nierekomendowane. Takie zagranie w przypadku słów kluczowych 'let' oraz 'const' zakończy się błędem. Ale dzięki temu mamy pewność, że nie nadpiszemy istniejących już zmiennych, a przynajmniej nie stanie się to nieświadomie - w linii 27 użycie słowa kluczowego 'var' sugeruje, jakbyśmy deklarowali nową zmienną, bo być może nie wiemy, że taka istnieje. W przypadku bardziej rozbudowanego kodu jest to jak najbardziej możliwe. No użycie 'let' lub 'const' pozwoli na uniknięcie takiego błędu.
 
 var name2;
 name2;
@@ -37,23 +39,28 @@ console.log(name2)
 
 // let
 let user = 'przeprogramowani';
-// wartość takiej zmiennej możemy również nadpisać j.w.
+// wartość takiej zmiennej możemy również nadpisać j.w. w przypadku 'var'
 
 user = 'overment';
 // ale przy ponownej deklaracji zmiennej z tym samym identyfikatorem i słowem kluczowym 'let' otrzymamy błąd SyntaxError
 
 // let user = 'nice try.';
 
-// SyntaxError = "identyfikator user jest już obecnie wykorzystywany"
+// SyntaxError = "identyfikator 'user' jest już obecnie wykorzystywany"
 
 
 // const
 const email = 'przeprogramowani@gmail.com';
 // deklarowanie zmiennej wygląda dokładnie tak samo, z tą różnicą, że nie możemy nadpisywać wartości zmiennej
 
-// email = 'kontakt@przeprogramowani.pl';
-
+// email = 'kontakt@przeprogramowani.pl'; // Assignment to constant variable
 // błąd informuje nas, że "nie możemy tak robić"
+
+// aby wykorzystać wartość zmiennej należy się odwołać do jej identyfikatora: np. dla "name" to: console.log(name);
+
+// VAR / LET / CONST
+
+// najlepiej zapamiętać to tak: ze słowa kluczowego 'const' korzystamy za każdym razem kiedy deklarujemy zmienną, chyba że mamy do czynienia z sytuacją, gdy wiemy,że wartość tej zmiennej musi się zaraz zmienić to wtedy korzystamy z 'let'. Natomiast o słowie kluczowym 'var' wiedzmy, że istnieje i że nie warto go obecnie używać. M.in. dlatego, że możemy deklarować zmienne o tym samym identyfikatorze.
 
 
 // BLOCK SCOPE
@@ -63,13 +70,21 @@ for (var i=0; i < 10; i++) {
 }
 console.log(`Outside loop i = ${i}`);
 
-// użyliśmy tu słowa kluczowego 'var' i zmienna po wykonaniu pętli ma teraz wartość 10, bo pętla wykonała się 9 razy i za każdym razem zwiększaliśmy wartość o 1 możemy nie być świadomi, że zmienna była używana w pętli i poza nią ma inną wartość i może to doprowadzić do cichego błędu, o którym nie będziemy mieli pojęcia
+// Tutaj mamy do czynienia z tzw. pętlą. Linia 69 wykona się 10 razy i zależy to od wartości zmiennej i, która jest zadeklarowana w linii 68 (i=0; i<10; i++). Za każdym razem, gdy pętla będzie się wykonywać wartość zmiennej i zostanie zwiększona o 1. Z pomocą console.log wyświetlamy tą wartość i spodziewamy się tutaj liczenia od 0 do 9. I faktycznie tak jest. 
+// Natomiast zwróć uwagę, że poza pętlą zmienna i ma wartość ustawioną na 10 (linia 71). Wynika to z tego, że pętla została wykonana 10 razy i za każdym razem zwiększaliśmy wartość zmiennej i o 1. Tak na prawdę może to stanowić problem, no bo skoro poza pętlą zmienna i posiada już jakąś wartość, a my niekoniecznie musimy wiedzieć, że gdzieś już ktoś wykorzystywał tą zmienną w pętli - możemy być tym faktem zaskoczeni i może doprowadzić do jakiegoś cichego błędu, którego nawet nie będziemy świadomi.
 
-// dlatego lepiej zadeklarować zmienną (j) za pomocą let:
+// Pytanie: Co możemy z tym zrobić?
+
+// previous comment: // użyliśmy tu słowa kluczowego 'var' i zmienna po wykonaniu pętli ma teraz wartość 10, bo pętla wykonała się 9 razy i za każdym razem zwiększaliśmy wartość o 1 możemy nie być świadomi, że zmienna była używana w pętli i poza nią ma inną wartość i może to doprowadzić do cichego błędu, o którym nie będziemy mieli pojęcia //
+
+// Odpowiedź: Zauważ, że mamy tutaj nawiasy klamrowe {} - fragment kodu pomiędzy nimi nazywamy blokiem. No i skoro zmienne zadeklarowane słowami kluczowymi 'let' oraz 'const' posiadają zakres blokowy, możemy zamienić słowo kluczowe 'var' na 'let' i wszystko będzie w porządku. Pętla zadziała tak jak tego oczekiwaliśmy, natomiast sama zmienna poza tą pętlą nie będzie dostępna. Oczywiście jak się domyślasz nie możemy użyć słowa kluczowego 'const' bo zmienna 'j' zmienia się w czasie, za każdym obrotem pętli wzrasta o 1.
+
+// // lepiej zadeklarować zmienną 'j' ('j' - wcześniejsze 'i') za pomocą let zamiast var i wszystko będzie w porządku:
 
 for (let j=0; j < 10; j++) {
     console.log(j);
 }
-console.log(`Outside loop j = ${j}`)
+console.log(`Outside loop j = ${j}`);
 
-// zmienna zadeklarowana let ma zakres blokowy, czyli nie jest dostępna poza blokiem funkcji ( poza nawiasami {} ) nie możemy też użyć const, bo zmienna się zmienia za każdym obrotem pętli
+
+// // zmienna zadeklarowana let ma zakres blokowy, czyli nie jest dostępna poza blokiem funkcji ( poza nawiasami {} ) nie możemy też użyć const, bo zmienna się zmienia za każdym obrotem pętli
